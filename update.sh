51e12bb23ab51b07dfb5dc0559bb13eb8124cfaa
Yoshiya Hinosawa <stibium121@gmail.com>
2016-11-13 13:22:13 +0900

    chore: delete all temporarily

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./node_modules/.bin/rimraf build
-git clone -b master https://github.com/nodejsjp/nodejsjp.github.com.git build
-npm run build
-cd build
-git add .
-git commit -m 'chore(site): update build'
-git push origin HEAD
