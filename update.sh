b717104f20924ee2a662592595c0d5f0ccba2ead
Yoshiya Hinosawa <stibium121@gmail.com>
2019-05-20 17:58:36 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ git clone -b master https://github.com/nodejsjp/nodejsjp.github.com.git build
 npm run build
 cd build
 git add .
-git commit -m 'chore(site): update build [skip ci]'
+git commit -m 'chore(site): automated update from circleci'
 git push origin HEAD
